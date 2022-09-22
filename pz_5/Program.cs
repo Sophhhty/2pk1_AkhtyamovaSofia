0dbc4cf877272bbd258d7ca26d112ff219622ae1
Sophhhty <sofiaakhtyamova10@mail.ru>
2022-09-22 18:27:52 +0300

    text edit

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -6,37 +6,40 @@ namespace pz_5
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("задача 1");
-            //int a = 0;
-            //for (a = 0; a <= 100; a+=2)
-            //{
-            //    Console.WriteLine(a);
-            //}
-            //Console.WriteLine("задача 2");
-            //char a = 'a';
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.Write(a);
-            //    a++;
-            //}
-            //Console.WriteLine("задача 3");
-            //char a = '#';
-            //for (var i = 0; i < 5; i++)
-            //{
-            //    for (var j = 0; j < 3; j++)
-            //    {
-            //        Console.Write(a);
-            //    }
-            //    Console.WriteLine();
-            //}
-            //Console.WriteLine("задание 4");
-            //for (int i = 0; i <= 100; i += 1)
-            //{
-            //    if (i % 3 == 0)
-            //        Console.WriteLine($"{i}");
-            //}
-            
+            Console.WriteLine("задача 1");
+            int a = 0;
+            for (a = 0; a <= 100; a += 2)
+            {
+                Console.WriteLine(a);
+            }
+            Console.WriteLine("задача 2");
+            char c = 'a';
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(c);
+                c++;
+            }
+            Console.WriteLine("задача 3");
+            char b = '#';
+            for (var i = 0; i < 5; i++)
+            {
+                for (var j = 0; j < 3; j++)
+                {
+                    Console.Write(b);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("задание 4");
+            for (int i = 0; i <= 100; i += 1)
+            {
+                if (i % 3 == 0)
+                    Console.WriteLine($"{i}");
+            }
+            Console.WriteLine("задание 5");
+            for (int i = 1, j = 40; j - i != 10; i++, j--)
+            {
+                Console.WriteLine(i + " " + j);
+            }
         }
-
     }
 }
