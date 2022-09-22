3e3c9ecfab0d217dc92a8cbc2407200e048de2f7
Sophhhty <sofiaakhtyamova10@mail.ru>
2022-09-22 09:49:05 +0300

    text exit

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace pz_5
+﻿using System.Globalization;
+
+namespace pz_5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("работа с git");
+            //Console.WriteLine("задача 1");
+            //int a = 0;
+            //for (a = 0; a <= 100; a+=2)
+            //{
+            //    Console.WriteLine(a);
+            //}
+            //Console.WriteLine("задача 2");
+            //char a = 'a';
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write(a);
+            //    a++;
+            //}
+            //Console.WriteLine("задача 3");
+            //char a = '#';
+            //for (var i = 0; i < 5; i++)
+            //{
+            //    for (var j = 0; j < 3; j++)
+            //    {
+            //        Console.Write(a);
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.WriteLine("задание 4");
+            //for (int i = 0; i <= 100; i += 1)
+            //{
+            //    if (i % 3 == 0)
+            //        Console.WriteLine($"{i}");
+            //}
+            
         }
+
     }
 }
