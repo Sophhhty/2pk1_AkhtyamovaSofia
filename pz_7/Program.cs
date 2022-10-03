fd736cc56a66eadb43ee01dc39dd6d86154be1e9
Sophhhty <112803551+Sophhhty@users.noreply.github.com>
2022-10-03 10:11:19 +0300

    Add files via upload

---
+++ pz_7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace pz_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10]; //объявление одномерного массива
+            for (int i = 0; i < 10; i++) //запись элементов массива
+                a[i] = int.Parse(Console.ReadLine()); //ввод 
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++) //вычислить сумму 
+            {
+                sum += a[i];
+            }
+            Console.WriteLine(sum);//вывод
+        }
+    }
+}
