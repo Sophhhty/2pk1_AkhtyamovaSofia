1f5123cf8bf00191f7d93601c2bb459e3487f3df
Sophhhty <112803551+Sophhhty@users.noreply.github.com>
2022-11-14 08:45:03 +0300

    Add files via upload

---
+++ Pz_12/Pz_12/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Pz_12
+{
+    internal class Program
+    {
+       public static void GetArea( double a, double b)
+        {
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            double g = ((a * b) / 2);
+            double h = ((a + b - c) / 2);
+            Console.WriteLine("Гипотенуза = " + c);
+            Console.WriteLine("Площадь = " + g);
+            Console.WriteLine("Радиус = " + h);
+        }
+        
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значения катетов");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            GetArea(x, y);
+            Console.WriteLine();
+        }
+    }
+}
