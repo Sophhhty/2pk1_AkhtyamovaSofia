cbe6b6f5064a3209fcf0bd13fc0fdbb5cb425d7c
Sophhhty <112803551+Sophhhty@users.noreply.github.com>
2022-11-27 20:30:10 +0300

    Add files via upload

---
+++ PZ_15/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+
+namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStream = new FileStream(@"D:\newfile.txt", FileMode.Append);
+
+            Console.WriteLine("Введите произвольную строку в ваш файл:");
+            string fi = Console.ReadLine();
+            StreamWriter streamwriter = new StreamWriter(fileStream);
+            streamwriter.Write(fi);
+            fileStream.Close();
+
+            string[] str = { ".опа", "Привет", "помогите", "хочуспать" };
+
+            int count = File.ReadAllLines(@"D:\newfile.txt").Length;
+
+            for (int i = 0; i < count; i++)
+            {
+                string s = File.ReadAllLines(@"D:\newfile.txt").Skip(i).FirstOrDefault();
+
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (s.Contains(str[j]))
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+
+
+
+        }
+    }
+}
