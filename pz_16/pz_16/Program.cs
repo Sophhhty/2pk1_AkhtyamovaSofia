62367d6ae4c23711d18e95af391d029a152e131f
Sophhhty <112803551+Sophhhty@users.noreply.github.com>
2022-12-10 07:47:04 +0300

    задание сделано частичнов
    в течение выходных доделаю!!

---
+++ pz_16/pz_16/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace pz_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fileStream = new FileStream(@"D:\newfl.txt", FileMode.Open, FileAccess.Read);
+        
+            Console.WriteLine("Введите произвольный текст в ваш файл:");
+           
+            StreamWriter stream = new StreamWriter(fileStream);
+    fileStream.Close();
+          
+            int count = File.ReadAllLines(@"D:\newfl.txt").Length; 
+            Console.WriteLine(count);
+            string[] txt = File.ReadAllLines(@"D:\newfl.txt");
+            int a = 0;
+            for (int i = 0; i < txt.Length; i++)
+            {
+                txt[i];
+                int b = txt[i].Split().Length;
+                a += b;
+            }
+
+            // = txt.Split(" ");
+            //    int txt = File.ReadAllText(@"D:\newfl.txt").Length;
+            //    Console.WriteLine(txt);
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
