6d8bc0af5e4f6cb898dc5d3034d8099b9cbf7484
Sophhhty <112803551+Sophhhty@users.noreply.github.com>
2023-02-09 10:51:50 +0300

    Add files via upload
    fix method in PZ_21(ChangeSigns)

---
+++ PZ_22/PZ_22/FractionNumber.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_21
+{
+   
+    internal class FractionNumber
+    {
+        public double dividend = 0;
+        public double divider = 0;
+        static char sign;
+        public static int plusValue;
+        public static int minusValue;
+        public double Dividend() { return dividend; }
+        public double Divider() { return divider; }
+
+        public FractionNumber() //конструктор 
+        {
+            dividend= 1;
+            divider= 1;
+
+
+        }
+        public FractionNumber(int dividend, int divider)// конструктор
+        {
+
+            if (dividend > 0 & divider < 0)
+            {
+                minusValue++;
+            }
+            else if (dividend > 0 & divider > 0)
+            {
+                plusValue++;
+            }
+            else if (dividend < 0 & divider < 0)
+            {
+                plusValue++;
+            }
+            else if (dividend < 0 & divider > 0)
+            {
+                minusValue++;
+            }
+
+            this.dividend = dividend;
+            this.divider = divider;
+            if (divider == 0)//усл-е знаменатель = 0 и числитель целое число
+            {
+                Console.WriteLine("на 0 делить нельзя");
+                this.dividend = Math.Abs(dividend);
+            }
+            else if (dividend % divider == 0)
+            {
+                this.dividend = Math.Abs(dividend);
+            }
+        }
+
+        public override string ToString()//вывод дроби
+        {
+            
+           ChangeSigns();
+           
+            return $"({sign}) " + dividend.ToString() + "/" + divider.ToString();
+        }
+
+        //метод со знаком
+        public void ChangeSigns()
+        {
+            if ((dividend > 0 & divider < 0) || (dividend < 0 & divider > 0))
+            {
+                sign = '-';
+                //Console.WriteLine($"{dividend}\n{sign} --\n {dividend}");
+            }
+            else
+            {
+                sign = '+';
+                //Console.WriteLine($"{dividend}\n{sign} --\n {dividend}");
+            }
+        } 
+    }
+}
---
+++ PZ_22/PZ_22/Print.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_21
+{
+    internal class Print
+    {
+        static void Main(string[] args)
+        {
+            
+            FractionNumber fn = new FractionNumber(-6,-7);
+            Console.WriteLine(fn);
+            FractionNumber fn1 = new FractionNumber(4, 6);
+            Console.WriteLine(fn1);
+            FractionNumber fn2 = new FractionNumber(6, -7);
+            Console.WriteLine(fn2);
+            FractionNumber fn3 = new FractionNumber(4, 6);
+            Console.WriteLine(fn3); 
+            FractionNumber fn4 = new FractionNumber(-6, -7);
+            Console.WriteLine(fn4);
+
+            Console.WriteLine($"Количество положительных дробных чисел: {FractionNumber.plusValue}");
+            Console.WriteLine($"Количество отрицательных дробных чисел: {FractionNumber.minusValue}");
+
+        }
+
+    }
+}
