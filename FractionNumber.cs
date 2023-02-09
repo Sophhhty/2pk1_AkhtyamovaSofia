2c6214fb3d6fde34c4889181d78374cc90425d2f
Sophhhty <112803551+Sophhhty@users.noreply.github.com>
2023-02-09 11:39:29 +0500

    Add files via upload

---
+++ FractionNumber.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_21
+{
+   
+    internal class FractionNumber
+    {
+        public double dividend = 0;
+        public double divider = 0;
+        static char sign;
+        static int counter;
+        static int GetCounter() { return counter; }
+        public double Dividend() { return dividend; }
+        public double Divider() { return divider; }
+
+        public FractionNumber() //конструктор 
+        {
+            dividend= 1;
+            divider= 1;
+
+
+        }
+        public FractionNumber(int dividend, int divider)// конструктор
+        {
+            
+            this.dividend = dividend;
+            this.divider = divider;
+            if (divider == 0)//усл-е знаменатель = 0 и числитель целое число
+            {
+                Console.WriteLine("на 0 делить нельзя");
+                this.dividend = Math.Abs(dividend);
+            }
+            else if (dividend % divider == 0)
+            {
+                this.dividend = Math.Abs(dividend);
+            }
+        }
+
+        public override string ToString()//вывод дроби
+        {
+            
+            GetNumber();
+           
+            return sign + dividend.ToString() + "/" + divider.ToString();
+        }
+
+        //метод с выбором знака, который не работает:(
+        public void GetNumber()
+        {
+            if ((dividend > 0 & divider < 0) || (dividend < 0 & divider > 0))
+            {
+                sign = '-';
+                //Console.WriteLine($"{dividend}\n{sign} --\n {dividend}");
+            }
+            else
+            {
+                sign = '+';
+                //Console.WriteLine($"{dividend}\n{sign} --\n {dividend}");
+            }
+        } 
+    }
+}
---
+++ Print.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_21
+{
+    internal class Print
+    {
+        static void Main(string[] args)
+        {
+            FractionNumber fn = new FractionNumber(-6, -8);
+            Console.WriteLine(fn);
+            FractionNumber fn1 = new FractionNumber(4, 6);
+            Console.WriteLine(fn1);
+            
+           
+
+        }
+
+    }
+}
